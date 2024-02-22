6ad287ba933d83db6df447fced6953c980eba5d5
W-ptr <wisnu.putra001@binus.ac.id>
2024-02-22 10:18:36 +0700

    new file:   app.mjs

---
+++ app.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+
+app.get('/',(req,res)=>{
+    res.status(200).send("<h1>WattWiser</h1>")
+});
+
+// app.post('/calculate',(req,res)=>{
+//     const obj = req.body;
+// });
+
+app.listen(port,()=>{
+    console.log(`Listening to port:${port}`);
+});
