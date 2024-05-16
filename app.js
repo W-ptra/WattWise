dea67ef739cf33d68f1a7f763a3d494ef95b5694
W-ptr <wisnu.putra001@binus.ac.id>
2024-05-16 19:23:30 +0700

    change to ejs

--- app.js
+++ app.js
@@ -2,28 +2,35 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 80;
+require("dotenv").config();
+const server_name = process.env.SERVER_NAME;
 
+app.set("view engine","ejs");
 app.use(express.json());
 app.use(cors({ origin: '*' }));
-app.use(express.static("web"));
+app.use(express.static("views"));
 
 app.get('/',(req,res)=>{
-    res.status(200).redirect("/home")
+    res.status(301).redirect("/home")
 });
 
 app.get('/home', (req, res) => {
-    res.sendFile('home/home.html', { root: __dirname + '/web' });
+    //res.sendFile('home/home.html', { root: __dirname + '/web' });
+    res.status(200).render('home/home',{server_identity: server_name});
 });
 
 app.get('/service', (req, res) => {
-    res.sendFile('second_page/index.html', { root: __dirname + '/web' });
+    //res.sendFile('second_page/index.html', { root: __dirname + '/web' });
+    res.status(200).render('service/index',{server_identity: server_name});
 });
 
 app.get('/product', (req, res) => {
-    res.sendFile('product/product.html', { root: __dirname + '/web' });
+    //res.sendFile('product/product.html', { root: __dirname + '/web' });
+    res.status(200).render('product/product',{server_identity: server_name});
 });
 app.get('/contact', (req, res) => {
-    res.sendFile('contact/contact.html', { root: __dirname + '/web' });
+    //res.sendFile('contact/contact.html', { root: __dirname + '/web' });
+    res.status(200).render('contact/contact',{server_identity: server_name});
 });
 
 app.listen(port,"0.0.0.0",()=>{
